1e81dc6aec2e3f57b4957068851966e919dcfa3d
Lev Chelyadinov <leva181777@gmail.com>
2025-02-16 00:05:42 +0100

    Set up Drizzle

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
+import "dotenv/config";
 import { serve } from "@hono/node-server";
+import { drizzle } from "drizzle-orm/libsql";
 import { Hono } from "hono";
+
+if (!process.env.DB_FILE_NAME) {
+	throw new Error("Env DB_FILE_NAME is not defined, see .env.example");
+}
+
+const db = drizzle(process.env.DB_FILE_NAME);
 
 const app = new Hono();
 
