cc47fe8ecc5f4faa44b75b927da1224642e76688
Lev Chelyadinov <leva181777@gmail.com>
2025-02-16 00:33:40 +0100

    Set up JWT authorization

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,25 @@ import "dotenv/config";
 import { serve } from "@hono/node-server";
 import { drizzle } from "drizzle-orm/libsql";
 import { Hono } from "hono";
+import { jwt } from "hono/jwt";
+import type { JwtVariables } from "hono/jwt";
 
 if (!process.env.DB_FILE_NAME) {
 	throw new Error("Env DB_FILE_NAME is not defined, see .env.example");
 }
+if (!process.env.JWT_SECRET) {
+	throw new Error("Env JWT_SECRET is not defined, see .env.example");
+}
 
 const db = drizzle(process.env.DB_FILE_NAME);
 
-const app = new Hono();
+const app = new Hono<{ Variables: JwtVariables }>();
+app.use(
+	"/*",
+	jwt({
+		secret: process.env.JWT_SECRET,
+	}),
+);
 
 app.get("/", (c) => {
 	return c.text("Hello Hono!");
