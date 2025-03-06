789b55f091960120ca349e2d806a83f7a13249b6
Lev Chelyadinov <leva181777@gmail.com>
2025-03-06 21:07:47 +0100

    Add a way to customize the port

--- src/cli.ts
+++ src/cli.ts
@@ -1,23 +1,31 @@
 import "dotenv/config";
 import { serve } from "@hono/node-server";
 
-import { object, parse, string } from "valibot";
+import {
+	number,
+	object,
+	optional,
+	parse,
+	pipe,
+	string,
+	transform,
+} from "valibot";
 import { factory } from "./factory.js";
 
 const Environment = object({
 	DB_FILE_NAME: string(),
 	JWT_SECRET: string(),
+	PORT: pipe(optional(string(), "3000"), transform(Number.parseInt), number()),
 });
 const env = parse(Environment, process.env);
 
 const app = factory.createApp();
 
-const port = 3000;
-console.log(`Server is running on http://localhost:${port}`);
+console.log(`Server is running on http://localhost:${env.PORT}`);
 
 serve({
 	fetch(request, httpBindings) {
 		return app.fetch(request, { ...env, ...httpBindings });
 	},
-	port,
+	port: env.PORT,
 });
